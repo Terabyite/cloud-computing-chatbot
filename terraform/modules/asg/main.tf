48edd77c87ad0bfc06495f38538ed6b8ed7008e1
terabyte <terabeautyworld@gmail.com>
2025-12-03 20:29:46 +0100

    Deploy

--- terraform/modules/asg/main.tf
+++ terraform/modules/asg/main.tf
@@ -15,29 +15,6 @@ variable "iam_instance_profile_name" {
   type        = string
 }
 
-# Instance role for SSM Session Manager (so you can shell in without public SSH)
-resource "aws_iam_role" "ec2_role" {
-  name = "${var.project}-ec2-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = { Service = "ec2.amazonaws.com" },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_core" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "${var.project}-ec2-profile"
-  role = aws_iam_role.ec2_role.name
-}
-
 
 # Launch Template
 resource "aws_launch_template" "lt" {
